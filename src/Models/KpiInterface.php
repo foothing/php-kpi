6bd3f8ea159c956cf07a0484ef4a201c68c9006f
brazorf <brazorf@users.noreply.github.com>
2016-09-20 16:02:25 +0200

    chore: improve kpi interface.

--- src/Models/KpiInterface.php
+++ src/Models/KpiInterface.php
@@ -5,6 +5,9 @@ interface KpiInterface {
     public function getId();
     public function getName();
     public function getFormula();
+    public function getThresholds();
+
+    // @TODO move in trait?
     public function quantizeValue($value);
 
 }
--- src/Repositories/KpiRepositoryInterface.php
+++ src/Repositories/KpiRepositoryInterface.php
@@ -1,8 +1,10 @@
 <?php namespace Foothing\Kpi\Repositories;
 
+use Foothing\Kpi\Models\KpiInterface;
+
 interface KpiRepositoryInterface {
 
     public function all();
-    public function cache();
+    public function store(KpiInterface $kpi, $value);
 
 }
