b426388cd57691acb02a4a2da5d3b87482e80488
brazorf <brazorf@users.noreply.github.com>
2017-02-14 14:49:25 +0100

    fix: kpi repository interface missing findOneBy().

--- src/Repositories/KpiRepositoryInterface.php
+++ src/Repositories/KpiRepositoryInterface.php
@@ -5,6 +5,7 @@ use Foothing\Kpi\Models\KpiInterface;
 interface KpiRepositoryInterface {
 
     public function all();
+    public function findOneBy($propertyName, $propertyValue);
     public function store(KpiInterface $kpi, $value);
 
 }
